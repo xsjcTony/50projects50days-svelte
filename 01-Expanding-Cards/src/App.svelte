8a22e6783e9ce529102329c43bdfbe54ba727cee
Aelita <xsjctony@126.com>
2023-01-26 03:32:06 +1100

    Svelte Starter Template

---
+++ 01-Expanding-Cards/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Counter from './components/Counter.svelte'
+</script>
+
+<main>
+  <h1>Svelte Template</h1>
+
+  <div>
+    <Counter />
+  </div>
+</main>
+
+<style>
+
+</style>
